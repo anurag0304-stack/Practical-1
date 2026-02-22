4e49643334e015881ed290cc58c8fba371421e21
anurag0304-stack <anurag720870@gmail.com>
2026-02-22 14:56:12 +0530

    Add files via upload

--- Practical1.py
+++ Practical1.py
@@ -6,17 +6,17 @@ x = np.array([[1, 2], [2, 3], [3, 1], [6, 5], [7, 7], [8, 6]])
 y = np.array([0, 0, 0, 1, 1, 1])
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(x, y)
-new_point = np.array([[4, 4]]) 
+new_point = np.array([[4, 4]])
 prediction = knn.predict(new_point)[0]
 plt.figure(figsize=(8, 6))
 plt.scatter(x[y==0,0], x[y==0,1], c='blue', label='Sunny',s=100)
 plt.scatter(x[y==1,0], x[y==1,1], c='red', label='Rainy',s=100)
 plt.scatter(
-    new_point[0, 0], 
-    new_point[0, 1], 
-    markers="*", 
-    s=200, 
-    label='New Prediction', 
+    new_point[0, 0],
+    new_point[0, 1],
+    marker="*",
+    s=200,
+    label='New Prediction',
     c='green'
     )
 plt.xlabel('temperture(°C)')
@@ -28,4 +28,5 @@ plt.show()
 if prediction == 0:
     print("The predicted weather condition for the new point is: Sunny")
 else:
-    print("The predicted weather condition for the new point is: Rainy")    
+    print("The predicted weather condition for the new point is: Rainy")
+    
